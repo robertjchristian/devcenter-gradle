dadca056b4e08a826181ccab3c4d9512bde49c3c
Robert Christian <robertjchristian@gmail.com>
2013-06-08 18:01:32 -0700

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,16 +9,7 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java built with Gradle!\n");
+        resp.getWriter().print("Hello from Java built with Gradle deployed from intellij!\n");
     }
 
-    public static void main(String[] args) throws Exception{
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
-        server.start();
-        server.join();   
-    }
 }
